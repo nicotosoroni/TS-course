033860eab0320b9a002471cb07c3b5ce67988684
Nicolas Francisco Tosoroni <nicolas.tosoroni@intive.com>
2024-06-06 17:53:04 -0300

    feat: add google map with API key

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,22 @@ import { Company } from './Company';
 const user = new User();
 const company = new Company();
 
-console.log(company);
+// hecho en el curso curso
+// new google.maps.Map(document.getElementById('map') as HTMLElement, {
+//   zoom: 1,
+//   center: { lat: 0, lng: 0 },
+// });
+
+// porporcionada por google
+let map: google.maps.Map;
+async function initMap(): Promise<void> {
+  const { Map } = (await google.maps.importLibrary(
+    'maps'
+  )) as google.maps.MapsLibrary;
+  map = new Map(document.getElementById('map') as HTMLElement, {
+    zoom: 1,
+    center: { lat: 0, lng: 0 },
+  });
+}
+
+initMap();
